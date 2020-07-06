cc4fecad84d2de10d7bb567d84032d7e221cc767
Mat <fangaas@gmail.com>
2020-07-06 18:02:39 +0200

    Base project setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        This is the app
+    </div>
+</template>
+
+<script>
+
+import axios from 'vue-cli-plugin-axios'
+import { gsap } from 'gsap'
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus">
+    @import "assets/stylus/app.styl"
+</style>

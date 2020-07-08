6b0b04e052ebc14c7c54892ad1967f6c2e6bee57
Mat <fangaas@gmail.com>
2020-07-08 10:27:05 +0200

    Additional functionality & adjustments

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
     <div id="app">
-        <div class="container">
+        <div class="container" ref="slideContainer">
             <div class="slide-wrapper" ref="slideWrapper">
-                <div class="slide login">
+                <div class="slide login" ref="slideLogin">
                     <input type="text" v-model="username" class="login__input login__input--user t-input" placeholder="Username">
                     <input type="password" v-model="password" class="login__input login__input-password t-input" placeholder="Password">
-                    <button @click="loginUser" class="button login__button t-text" :class="{ 'login__button--active': username !== '' && password !== '' }">Login</button>
-                    <span class="login__message">{{ loginMessage }}</span>
+                    <button @click="loginUser" class="button login__button" :class="{ 'login__button--active': username !== '' && password !== '' }">Login</button>
+                    <span class="login__message t-error">{{ loginErrorMessage }}</span>
                 </div>
                 <div class="slide list">
-                    <span class="t-text" v-if="loadingList">Loading Cameras...</span>
-                    <div class="list__container" v-if="!loadingList">
-                        Camera list
+                    <span v-if="loadingList">Loading Cameras...</span>
+                    <div class="list__container" v-else>
+                        <div class="list__item" v-for="(item, i) in cameraList" :key="i" @click="retrieveSingleCamera(item.cameraId)">
+                            <span class="list__item-number">{{ i + 1 }}</span>
+                            <span class="list__item-name">{{ item.name }}</span>
+                            <span class="list__item-id t-id">ID: {{ item.cameraId }}</span>
+                        </div>
                     </div>
                 </div>
                 <div class="slide details">
-                    <span class="t-text" v-if="loadingList">Loading Cameras...</span>
-                    <div class="list__container" v-if="!loadingList">
-                        Camera list
+                    <button @click="slidePanel(1)" class="button details__button t-back">Go back</button>
+                    <span v-if="loadingDetails">Loading Camera Details...</span>
+                    <div class="details__container t-details">
+                        <div class="details__list" v-if="cameraDetails">
+                            <span>Account ID: {{ cameraDetails.accountId }}</span>
+                            <span>Camera ID: {{ cameraDetails.cameraId }}</span>
+                            <span>Device Type ID: {{ cameraDetails.deviceTypeId }}</span>
+                            <span>Ethernet Address: {{ cameraDetails.ethMacAddress }}</span>
+                            <span>Name: {{ cameraDetails.name }}</span>
+                            <span>Zone ID: {{ cameraDetails.zoneId }}</span>
+                        </div>
                     </div>
                 </div>
             </div>
+            <transition name="fade">
+                <span class="logout-progress t-logout" v-if="loggingOut">Logging out...</span>
+                <button class="logout-button t-logout" v-else-if="loggedIn" @click="logOutUser">Log Out</button>
+            </transition>
+            <transition name="fade">
+                <div class="axios-error" v-if="axiosError">{{ axiosError }}</div>
+            </transition>
         </div>
     </div>
 </template>
@@ -35,77 +54,144 @@ export default {
 
     data: () => {
         return {
+            clientId: process.env.VUE_APP_CLIENT_ID,
+            clientSecret: process.env.VUE_APP_CLIENT_SECRET,
             username: '',
             password: '',
-            loginMessage: '',
+            loginErrorMessage: '',
             accessToken: null,
             loadingList: false,
             loadingDetails: false,
             cameraList: null,
             cameraDetails: null,
-            selectedCameraId: null
+            axiosError: null,
+            loggedIn: false,
+            loggingOut: false
         }
     },
 
+    mounted () {
+
+        new gsap.timeline()
+        .set(this.$refs.slideContainer, {
+            scale: 1.2
+        })
+        .set(this.$refs.slideLogin, {
+            opacity: 0,
+            y: 15
+        })
+        .to(this.$refs.slideContainer, .2, {
+            scale: 1
+        }, 'a')
+        .to(this.$refs.slideLogin, .4, {
+            opacity: 1,
+            y: 1
+        }, 'a+=.1')
+    },
+
     methods: {
-        loginUser () {
-            // const url = 'https://rest.cameramanager.com/oauth/token';
-            let clientId = 'dev_test';
-            let clientSecret = '3H1Bf6mCctIgpCuzvrnyekf3VhAUEnKJ';
 
-            const url = `https://rest.cameramanager.com/oauth/token?grant_type=password&scope=read&username=onlinedemo@cameramanager.com&password=demo1234&client_id=${clientId}&client_secret=${clientSecret}`;
+        newLoginUser () {
+            const params = new URLSearchParams(window.location.search)
+            const code = params.get('code');
 
-            this.axios.post(url, {})
-            .then(response => {
-                if(response && response.data) {
-                    this.accessToken = response.data.access_token
+            const url = `https://rest.cameramanager.com/oauth/token?grant_type=authorization_code&scope=read&code=${code}&redirect_uri=http://localhost:8080&client_id=${this.clientId}&client_secret=${this.clientSecret}`;
 
-                    this.loginMessage = loggedIn;
-
-                    this.slidePanel(1);
-                    this.retrieveAccountCameras();
+            this.axios.post(url, {
+                crossDomain: true,
+                headers: {
+                    Authorization: 'Basic 3H1Bf6mCctIgpCuzvrnyekf3VhAUEnKJ',
+                    Accept: 'application/json'
                 }
             })
-            .catch(error => {
-                debugger;
-                this.loginMessage = 'Error Logging In';
+            .then(response => {
+                callback(response);
             })
+            .catch(error => {
+            })
+
         },
 
-        slidePanel(showPanel) {
+        loginUser () {
+            // this.username = 'onlinedemo@cameramanager.com';
+            // this.password = 'demo1234';
 
+            this.loginErrorMessage = '';
+
+            const url = `https://rest.cameramanager.com/oauth/token?grant_type=password&scope=read&username=${this.username}&password=${this.password}&client_id=${this.clientId}&client_secret=${this.clientSecret}`;
+
+            this.makeAxiosRequest('post', url, response => {
+                if (response && response.data) {
+                    this.loggedIn = true;
+                    this.accessToken = response.data.access_token
+                    this.retrieveAccountCameras();
+                }
+            }, error => {
+                this.loginErrorMessage = 'Error Logging In';
+            });
+        },
+
+        slidePanel (showPanel) {
+            const slideAmount = -(100 / 3) * showPanel;
+
+            gsap.to(this.$refs.slideWrapper, 0.65, {
+                x: `${slideAmount}%`,
+                ease: 'Power3.easeInOut'
+            });
         },
 
         retrieveAccountCameras () {
-            // Works
             this.loadingList = true;
+            this.slidePanel(1);
 
-            const url = "https://rest.cameramanager.com/rest/v2.4/cameras";
+            const url = 'https://rest.cameramanager.com/rest/v2.4/cameras';
 
-            this.axios.get(url, {
+            this.makeAxiosRequest('get', url, response => {
+                if (response && response.data) this.cameraList = response.data;
+                this.loadingList = false;
+            });
+        },
+
+        retrieveSingleCamera (cameraId) {
+            this.loadingDetails = true;
+            this.slidePanel(2);
+
+            const url = `https://rest.cameramanager.com/rest/v2.4/cameras/${cameraId}`;
+
+            this.makeAxiosRequest('get', url, response => {
+                if (response && response.data) this.cameraDetails = response.data;
+                this.loadingDetails = false;
+            });
+        },
+
+        logOutUser () {
+            this.loggingOut = true;
+            const url = 'https://rest.cameramanager.com/rest/v2.0/users/self/tokens/current';
+
+            this.makeAxiosRequest('delete', url, response => {
+                this.slidePanel(0);
+                this.loggedIn = false;
+                this.loggingOut = false;
+            })
+        },
+
+        makeAxiosRequest (type, url, callback, errorCallback) {
+            this.axiosError = null;
+
+            this.axios[type](url, {
+                crossDomain: true,
                 headers: {
-                    Authorization: `Bearer ${this.accessToken}`
+                    Authorization: `Bearer ${this.accessToken}`,
+                    Accept: 'application/json'
                 }
             })
             .then(response => {
-                debugger;
-                this.loadingList = false;
+                callback(response);
             })
             .catch(error => {
-                debugger;
+                if (errorCallback) errorCallback(error);
+                else this.axiosError = 'Error making the request';
             })
-        },
-
-        makeAxiosRequest (url, callback) {
-
-        },
-
-        retrieveSingleCameraInfo () {
-
-        },
-
-        goBackToList () {
-
         }
     }
 }
